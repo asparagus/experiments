de621cc0f10a805dc3445815dd11150d65d4a9bd
asparagus <arielperezch@gmail.com>
2024-08-03 01:45:12 -0700

    Set up simple layer structure & streamlit app

---
+++ activations.py
@@ -0,0 +1,65 @@
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+from experiments.simplelayer import SimpleLayer
+
+
+SEED = 0
+
+
+if __name__ == "__main__":
+    st.title("Activations")
+    st.markdown(
+        """
+        The goal of this experiment is to validate a model using `SimpleLayer` where the activity
+        can remain somewhat constant with a variety of neuron activations.
+        """
+    )
+
+    with st.sidebar:
+        n = st.slider("n", min_value=1, max_value=100, value=50)
+        steps = st.slider("steps", min_value=10, max_value=100, value=50)
+
+        potential_decay = st.slider("potential_decay", min_value=0.0, max_value=1.0, value=0.9)
+        activation_threshold = st.slider("activation_threshold", min_value=0.0, max_value=1.0, value=0.8)
+        refractory_value = st.slider("refractory_value", min_value=-10.0, max_value=0.0, value=-0.2)
+
+    np.random.seed(SEED)
+    l = SimpleLayer(
+        n=n,
+        potential_decay=potential_decay,
+        activation_threshold=activation_threshold,
+        refractory_value=refractory_value,
+    )
+    activation_records = []
+    potential_records = []
+    for i in range(steps):
+        activations = l.activations()
+        potential_records.append(l.potential)
+        activation_records.append(activations)
+        l.tick(activations=activations)
+
+    all_activations = [
+        (idx[0], t)
+        for t, slice in enumerate(activation_records)
+        for idx in np.transpose(np.nonzero(slice))
+    ]
+    df = pd.DataFrame(
+        data=all_activations,
+        columns=["idx", "t"],
+    )
+    
+    act_fig = px.density_heatmap(df, x="t", y="idx", marginal_x="histogram", marginal_y="histogram", nbinsx=steps, nbinsy=n)
+    st.header("Current Results")
+    firing_period = steps * n / len(all_activations) if all_activations else float("inf")
+    average_activation = len(all_activations) / (n * steps) * 100
+    st.subheader(f"Firing period: {firing_period:.2f} cycles")
+    st.subheader(f"Average activation: {average_activation:.2f}%")
+    st.plotly_chart(act_fig, theme="streamlit", use_container_width=True)
+    st.caption("Neuron activations across time")
+    pot_fig = px.imshow(np.transpose(potential_records))
+    st.plotly_chart(pot_fig, theme="streamlit", use_container_width=True)
+    st.caption("Neuron potential across time")
---
+++ experiments/simplelayer.py
@@ -0,0 +1,38 @@
+"""SimpleLayer module."""
+from typing import Optional
+import numpy as np
+
+
+class SimpleLayer:
+    def __init__(
+            self,
+            n: int,
+            potential_decay: float = 0.9,
+            activation_threshold: float = 0.8,
+            refractory_value: float = -0.2
+    ):
+        self.n = n
+        self.potential_decay = potential_decay
+        self.activation_threshold = activation_threshold
+        self.refractory_value = refractory_value
+
+        self.potential = np.random.random(size=n)
+        self.thresholds = np.ones_like(self.potential) * activation_threshold
+
+        connections = np.random.random(size=[n, n]) * 2 - 1
+        self.excitatory_connections = np.where(connections > 0, connections, 0)
+        self.excitatory_connections = self.excitatory_connections/ np.linalg.norm(self.excitatory_connections, axis=1)[:, np.newaxis]
+        self.inhibitory_connections = np.where(connections < 0, connections, 0)
+        self.inhibitory_connections = self.inhibitory_connections / np.linalg.norm(self.inhibitory_connections, axis=1)[:, np.newaxis]
+
+    def activations(self) -> np.ndarray:
+        return (self.potential > self.thresholds).astype(float)
+
+    def tick(self, activations: Optional[np.ndarray] = None):
+        if activations is None:
+            activations = self.activations()
+        excitations = self.excitatory_connections.dot(activations)
+        inhibitions = self.inhibitory_connections.dot(activations)
+        computed_update = excitations + inhibitions
+        computed_potential = self.potential * self.potential_decay + np.where(computed_update > 0, computed_update, 0)
+        self.potential = np.where(activations > 0, self.refractory_value, computed_potential)
